84cfa27b48a30580087c164e37f25e3300380696
commit test on new mac <dhritiman.ganguly@outlook.com>
2017-12-19 00:57:26 -0500

    rename websites as applications

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,36 +8,36 @@ import {Routing} from './app.routing';
 import {HttpModule} from '@angular/http';
 import {FormsModule} from '@angular/forms';
 import {TestService} from './services/test.service.client';
-import { UserComponent } from './user/user.component';
-import { LoginComponent } from './login/login.component';
+// import { UserComponent } from './user/user.component';
+// import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './components/user/profile/profile.component';
 import { RegisterComponent } from './components/user/register/register.component';
 import { WebsiteNewComponent } from './components/website/website-new/website-new.component';
 import { WebsiteEditComponent } from './components/website/website-edit/website-edit.component';
 import { WebsiteListComponent } from './components/website/website-list/website-list.component';
-import { PageNewComponent } from './components/page/page-new/page-new.component';
-import { PageEditComponent } from './components/page/page-edit/page-edit.component';
-import { PageListComponent } from './components/page/page-list/page-list.component';
-import { WidgetChooserComponent } from './components/widget/widget-chooser/widget-chooser.component';
-import { WidgetEditComponent } from './components/widget/widget-edit/widget-edit.component';
-import { WidgetListComponent } from './components/widget/widget-list/widget-list.component';
-import { WidgetHeaderComponent } from './components/widget/widget-edit/widget-header/widget-header.component';
-import { WidgetImageComponent } from './components/widget/widget-edit/widget-image/widget-image.component';
-import { WidgetYoutubeComponent } from './components/widget/widget-edit/widget-youtube/widget-youtube.component';
+// import { PageNewComponent } from './components/page/page-new/page-new.component';
+// import { PageEditComponent } from './components/page/page-edit/page-edit.component';
+// import { PageListComponent } from './components/page/page-list/page-list.component';
+// import { WidgetChooserComponent } from './components/widget/widget-chooser/widget-chooser.component';
+// import { WidgetEditComponent } from './components/widget/widget-edit/widget-edit.component';
+// import { WidgetListComponent } from './components/widget/widget-list/widget-list.component';
+// import { WidgetHeaderComponent } from './components/widget/widget-edit/widget-header/widget-header.component';
+// import { WidgetImageComponent } from './components/widget/widget-edit/widget-image/widget-image.component';
+// import { WidgetYoutubeComponent } from './components/widget/widget-edit/widget-youtube/widget-youtube.component';
 import { LandingComponent } from './components/user/landing/landing.component';
 
 import { UserService } from './services/user.service.client';
 import {WebsiteService} from './services/website.service.client';
-import {PageService} from './services/page.service.client';
-import {WidgetService} from './services/widget.service.client';
-import { WidgetTextComponent } from './components/widget/widget-edit/widget-text/widget-text.component';
+// import {PageService} from './services/page.service.client';
+// import {WidgetService} from './services/widget.service.client';
+// import { WidgetTextComponent } from './components/widget/widget-edit/widget-text/widget-text.component';
 import { QuillEditorModule } from 'ngx-quill-editor';
-import { WidgetHtmlComponent } from './components/widget/widget-edit/widget-html/widget-html.component';
+// import { WidgetHtmlComponent } from './components/widget/widget-edit/widget-html/widget-html.component';
 import {SharedService} from './services/shared.service.client';
 import {AuthenticationService} from './services/authentication.service.client';
-import { FlickrImageSearchComponent } from './components/widget/widget-edit/widget-image/flickr-image-search/flickr-image-search.component';
-import {FlickrService} from './services/flickr.service.client';
-import {SortableDirective} from './directives/sortable.directive';
+// import { FlickrImageSearchComponent } from './components/widget/widget-edit/widget-image/flickr-image-search/flickr-image-search.component';
+// import {FlickrService} from './services/flickr.service.client';
+// import {SortableDirective} from './directives/sortable.directive';
 
 
 @NgModule({
@@ -46,27 +46,27 @@ import {SortableDirective} from './directives/sortable.directive';
     AppComponent,
     HomeComponent,
     TestComponent,
-    UserComponent,
-    LoginComponent,
+    // UserComponent,
+    // LoginComponent,
     ProfileComponent,
     RegisterComponent,
     WebsiteNewComponent,
     WebsiteEditComponent,
     WebsiteListComponent,
-    PageNewComponent,
-    PageEditComponent,
-    PageListComponent,
-    WidgetChooserComponent,
-    WidgetEditComponent,
-    WidgetListComponent,
-    WidgetHeaderComponent,
-    WidgetImageComponent,
-    WidgetYoutubeComponent,
+    // PageNewComponent,
+    // PageEditComponent,
+    // PageListComponent,
+    // WidgetChooserComponent,
+    // WidgetEditComponent,
+    // WidgetListComponent,
+    // WidgetHeaderComponent,
+    // WidgetImageComponent,
+    // WidgetYoutubeComponent,
     LandingComponent,
-    WidgetTextComponent,
-    WidgetHtmlComponent,
-    FlickrImageSearchComponent,
-    SortableDirective
+    // WidgetTextComponent,
+    // WidgetHtmlComponent,
+    // FlickrImageSearchComponent,
+    // SortableDirective
   ],
   imports: [
     BrowserModule,
@@ -76,7 +76,7 @@ import {SortableDirective} from './directives/sortable.directive';
     Routing
   ],
   // Client Side services here
-  providers: [ TestService, UserService, WebsiteService, PageService, WidgetService, SharedService, AuthenticationService, FlickrService],
+  providers: [ TestService, UserService, WebsiteService, SharedService, AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -10,21 +10,21 @@ import {LandingComponent} from './components/user/landing/landing.component';
 import {RegisterComponent} from './components/user/register/register.component';
 import {ProfileComponent} from './components/user/profile/profile.component';
 import {WebsiteListComponent} from './components/website/website-list/website-list.component';
-import {PageListComponent} from './components/page/page-list/page-list.component';
-import {WidgetListComponent} from './components/widget/widget-list/widget-list.component';
+// import {PageListComponent} from './components/page/page-list/page-list.component';
+// import {WidgetListComponent} from './components/widget/widget-list/widget-list.component';
 import {WebsiteNewComponent} from './components/website/website-new/website-new.component';
 import {WebsiteEditComponent} from './components/website/website-edit/website-edit.component';
-import {PageNewComponent} from './components/page/page-new/page-new.component';
-import {PageEditComponent} from './components/page/page-edit/page-edit.component';
-import {WidgetChooserComponent} from './components/widget/widget-chooser/widget-chooser.component';
-import {WidgetEditComponent} from './components/widget/widget-edit/widget-edit.component';
-import {WidgetHeaderComponent} from './components/widget/widget-edit/widget-header/widget-header.component';
-import {WidgetImageComponent} from './components/widget/widget-edit/widget-image/widget-image.component';
-import {WidgetYoutubeComponent} from './components/widget/widget-edit/widget-youtube/widget-youtube.component';
-import {WidgetTextComponent} from './components/widget/widget-edit/widget-text/widget-text.component';
-import {WidgetHtmlComponent} from './components/widget/widget-edit/widget-html/widget-html.component';
+// import {PageNewComponent} from './components/page/page-new/page-new.component';
+// import {PageEditComponent} from './components/page/page-edit/page-edit.component';
+// import {WidgetChooserComponent} from './components/widget/widget-chooser/widget-chooser.component';
+// import {WidgetEditComponent} from './components/widget/widget-edit/widget-edit.component';
+// import {WidgetHeaderComponent} from './components/widget/widget-edit/widget-header/widget-header.component';
+// import {WidgetImageComponent} from './components/widget/widget-edit/widget-image/widget-image.component';
+// import {WidgetYoutubeComponent} from './components/widget/widget-edit/widget-youtube/widget-youtube.component';
+// import {WidgetTextComponent} from './components/widget/widget-edit/widget-text/widget-text.component';
+// import {WidgetHtmlComponent} from './components/widget/widget-edit/widget-html/widget-html.component';
 import {AuthenticationService} from './services/authentication.service.client';
-import {FlickrImageSearchComponent} from './components/widget/widget-edit/widget-image/flickr-image-search/flickr-image-search.component';
+// import {FlickrImageSearchComponent} from './components/widget/widget-edit/widget-image/flickr-image-search/flickr-image-search.component';
 
 
 
@@ -39,18 +39,18 @@ const APP_ROUTES: Routes = [
   {path : 'user/:userId/website' , component: WebsiteListComponent},
   {path : 'user/:userId/website/new', component: WebsiteNewComponent},
   {path : 'user/:userId/website/:wid', component: WebsiteEditComponent},
-  {path : 'user/:userId/website/:wid/page' , component: PageListComponent},
-  {path : 'user/:userId/website/:wid/page/new' , component: PageNewComponent},
-  {path : 'user/:userId/website/:wid/page/:pid' , component: PageEditComponent},
-  {path : 'user/:userId/website/:wid/page/:pid/widget' , component: WidgetListComponent},
-  {path : 'user/:userId/website/:wid/page/:pid/widget/new' , component: WidgetChooserComponent},
-  {path : 'user/:userId/website/:wid/page/:pid/widget/:wgid' , component: WidgetEditComponent},
-  {path : 'user/:userId/website/:wid/page/:pid/widget/new/heading' , component: WidgetHeaderComponent},
-  {path : 'user/:userId/website/:wid/page/:pid/widget/new/image' , component: WidgetImageComponent},
-  {path : 'user/:userId/website/:wid/page/:pid/widget/new/youtube' , component: WidgetYoutubeComponent},
-  {path : 'user/:userId/website/:wid/page/:pid/widget/new/text' , component: WidgetTextComponent},
-  {path : 'user/:userId/website/:wid/page/:pid/widget/new/html' , component: WidgetHtmlComponent},
-  {path : 'user/:userId/website/:wid/page/:pid/widget/new/image/search', component: FlickrImageSearchComponent}
+  // {path : 'user/:userId/website/:wid/page' , component: PageListComponent},
+  // {path : 'user/:userId/website/:wid/page/new' , component: PageNewComponent},
+  // {path : 'user/:userId/website/:wid/page/:pid' , component: PageEditComponent},
+  // {path : 'user/:userId/website/:wid/page/:pid/widget' , component: WidgetListComponent},
+  // {path : 'user/:userId/website/:wid/page/:pid/widget/new' , component: WidgetChooserComponent},
+  // {path : 'user/:userId/website/:wid/page/:pid/widget/:wgid' , component: WidgetEditComponent},
+  // {path : 'user/:userId/website/:wid/page/:pid/widget/new/heading' , component: WidgetHeaderComponent},
+  // {path : 'user/:userId/website/:wid/page/:pid/widget/new/image' , component: WidgetImageComponent},
+  // {path : 'user/:userId/website/:wid/page/:pid/widget/new/youtube' , component: WidgetYoutubeComponent},
+  // {path : 'user/:userId/website/:wid/page/:pid/widget/new/text' , component: WidgetTextComponent},
+  // {path : 'user/:userId/website/:wid/page/:pid/widget/new/html' , component: WidgetHtmlComponent},
+  // {path : 'user/:userId/website/:wid/page/:pid/widget/new/image/search', component: FlickrImageSearchComponent}
 
 
 ];
